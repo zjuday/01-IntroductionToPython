de60b82d32360d7dfddd908ab971bc4fa58b103e
DavidMutchler <mutchler@rose-hulman.edu>
2018-08-28 12:48:21 -0400

    Added a short exercise about the TYPE and VALUE of objects. Best done in class via a sort of live-coding.

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -1,15 +1,17 @@
 
-answer = 2 + 2
+answer = 2 ** 5
 print(answer * 100)
 
 ###############################################################################
 # TODO: 1.
 #   Read the 2 lines of code ABOVE this _TODO_.  That code:
-#     1. Computes 2 + 2, yielding the object that is the integer 4.
+#     1. Computes 2 raised to the 5th power, yielding the object that is
+#          the integer 32.
 #     2. Makes the name   answer   refer to that object.
-#     3. Looks up the object to which the name   answer  refers (i.e., 4).
-#     4. Multiplies that object (4) by 100 (hence computing the object 400).
-#     5. Prints the newly-computed object (400).
+#     3. Looks up the object to which the name   answer  refers (i.e., 32).
+#     4. Multiplies that object (32) by 100 (hence computing the object
+#          that is the integer 3,200).
+#     5. Prints the newly-computed object.  (It prints WITHOUT the comma.)
 #
 #   Make sure that you understand that those two lines do the above,
 #     ** ASKING QUESTIONS AS NEEDED. **
@@ -19,11 +21,63 @@ print(answer * 100)
 
 ###############################################################################
 # TODO: 2.
-#   Immediately below this _TODO_, write lines of code that together:
-#     - Computes the square root of the cosine of 0.75.
+#   Immediately below this _TODO_, write code that:
+#     - Computes 77 plus the cosine of 2.75.
+#         HINT: You will need to import the   math  module (library).
 #     - Stores that computed value using a name of your own choosing.
 #     - Prints the square root of that computed value.
+#   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
 
-# Compute and print the square root of ((41 * 88) + (4 * the cosine of 2))
+###############################################################################
+# TODO: 3.
+#   Immediately below this _TODO_, write code that computes and prints:
+#      the square root of ((41 * 88) + (4 * the cosine of 2))
+#   Use as few or as many intermediate names as you feel appropriate.
+#   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
+###############################################################################
 
+
+###############################################################################
+# TODO: 4.
+#   Compute the square root of 2 in two ways:
+#     - By using the   math.sqrt   function.
+#     - By raising 2 to the 0.5 power (using   **   for exponentiation).
+#   Print both expressions.
+#   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
+###############################################################################
+
+
+###############################################################################
+# TODO: 5.
+#   Every object has a TYPE and a VALUE.  For example,
+#   for the object that is computed by  math.sqrt(2):
+#      Its TYPE is float  (which is shorthand for "floating point number")
+#      Its VALUE is (approximately) 1.4142135623730951.
+#
+#   The TYPE of an object is important because it determines:
+#      -- what the object KNOWS, and
+#      -- what the object can DO.
+#
+#   The   type   function returns the TYPE of its argument.  For example,
+#       type(3.14)    returns the CLASS (synonym for TYPE) 'float'
+#   and so the code:
+#       print(type(3.14))
+#   will print     <class 'float'>
+#   Try it now!
+#   (Just enter   print(type(3.14))   below and run the program.)
+#
+#   Then write more code of the form   print(type(BLAH))
+#   to determine the type of each of the following objects:
+#       "hello"
+#       'hello'
+#       "a b c"
+#       "3 + 3"
+#       3 + 3
+#       "3" + "3"
+#       2 ** 100
+#       2.0 ** 100
+#       math.sin(8)
+#       math.sin
+#   Run your code (see what is printed!), then change the above _TODO_ to DONE.
+###############################################################################
