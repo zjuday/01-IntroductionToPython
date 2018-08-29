7a1ce48d21f31fcfce05b260caf9ea4b14ce23dc
DavidMutchler <mutchler@rose-hulman.edu>
2018-08-29 15:07:15 -0400

    strengthened and clarified.  May be too tough for students...

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -11,18 +11,22 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 
 ########################################################################
 # TODO: 2.
-#   Write code that:
-#     - constructs a SimpleTurtle with a  blue  Pen
-#     - makes the SimpleTurtle go straight up 200 pixels
-#     - tells the SimpleTurtle to move the PEN up.
-#     - tells the SimpleTurtle to jump to (100, -40)
-#     - tells the SimpleTurtle to move the PEN down.
-#     - tells the SimpleTurtle to move straight right 300 pixels.
+#   Write code that accomplishes the following:
+#     - Constructs a SimpleTurtle with a  blue  Pen.
+#     - Makes the SimpleTurtle go straight UP 200 pixels.
+#     - Tells the SimpleTurtle to make its pen go UP
+#          (so that the next movements do NOT leave a "trail")
+#          HINT: Use the "dot trick" to figure out how to do this.
+#     - Tells the SimpleTurtle to go to (100, -40).
+#     - Tells the SimpleTurtle to make its pen go DOWN
+#          (so that the next movements will return to leaving a "trail")
+#     - Makes the SimpleTurtle's pen have color "green" and thickness 10.
+#     - Tells the SimpleTurtle to go 150 pixels straight DOWN.
 #
 # Don't forget to:
-#     -- import rosegraphics and construct a TurtleWindow
+#     - import rosegraphics and construct a TurtleWindow
 #          at the BEGINNING of your code, and to
-#     -- ask your TurtleWindow to   close_on_mouse_click
+#     - ask your TurtleWindow to   close_on_mouse_click
 #          as the LAST line of your code.
 #   See the beginning and end of m4e_loopy_turtles for an example.
 #
