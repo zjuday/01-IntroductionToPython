860ff40460a3bdbf8f8187243798a6baf9ebf255
DavidMutchler <mutchler@rose-hulman.edu>
2018-11-18 09:53:44 -0500

    Modified to obey 72 limit on comment lengths.

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -1,4 +1,4 @@
-###############################################################################
+########################################################################
 #
 #   Do this WITH YOUR INSTRUCTOR (live in class):
 #
@@ -7,18 +7,21 @@
 #
 #   Then run this module by right clicking anywhere in this window
 #   and then selecting:
-#        Run 'name of file'      (i.e.  Run 'm1e_comments_strings...')
-#   Or, use the Windows keyboard shortcut:  Control + Shift + Function-F10
-#   (That is, hold the Control, Shift and Function keys down
-#   and press the F10 key.)
+#        Run 'name of file'
+#   (i.e.  Run 'm1e_comments_strings...')  Or, use the Windows keyboard
+#   shortcut:  Control + Shift + Function-F10.  (That is, hold the
+#   Control, Shift and Function keys down and press the F10 key.)
 #
-#   Confirm that you see the output in the  "Run"  window (to the right).
-#   Confirm that the output is what you expected, asking questions as desired.
+#   Confirm that you see the output in the  "Run"  window that appears.
+#   Confirm that the output is what you expected, asking questions
+#   as desired.
 #
-#   This module is just an example (m1e, note the 'e').  After you have read
-#   and run the code, feel free to play with it briefly, then move on to m2.
+#   This module is just an example (m1e, note the 'e').
+#   After you have read and run the code,
+#   feel free to play with it briefly, then move on to m2.
 #
-###############################################################################
+########################################################################
+
 
 print('Hello, World')
 print('hi there')
