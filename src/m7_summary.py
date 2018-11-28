b0242ee4dbcc94ef4e8e0c2d1d7fa5fbb1a7f1e6
zjuday <judayzj@rose-hulman.edu>
2018-11-28 01:42:19 -0500

    DONE

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,16 +2,16 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Zachary Juday.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
@@ -43,3 +43,16 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+nanners = rg.SimpleTurtle('turtle')
+nanners.pen = rg.Pen('blue', 5)
+nanners.left(90)
+nanners.forward(200)
+nanners.pen_up()
+nanners.go_to(rg.Point(100,-40))
+nanners.pen_down()
+nanners.pen = rg.Pen('green',10)
+nanners.right(180)
+nanners.forward(150)
+window.close_on_mouse_click()
